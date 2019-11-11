b49c0fcda5310315bfb791b20191d4cb3e219250
Will King <contact@wking.dev>
2019-11-11 17:02:18 -0600

    Finished up the site

--- wp-content/themes/power-for-today/inc/favicons.php
+++ wp-content/themes/power-for-today/inc/favicons.php
@@ -10,10 +10,7 @@ function pft_favicon() {
     echo '<link rel="apple-touch-icon" sizes="180x180" href="' . $root . 'apple-touch-icon.png">
     <link rel="icon" type="image/png" href="' . $root . 'favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="' . $root . 'favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="' . $root . 'site.webmanifest">
-    <link rel="mask-icon" href="' . $root . 'safari-pinned-tab.svg" color="#16a9a9">
-    <meta name="msapplication-TileColor" content="#16a9a9">
-    <meta name="theme-color" content="#ffffff">';
+    <link rel="manifest" href="' . $root . 'site.webmanifest">';
 }
 
 add_action( 'wp_head', 'pft_favicon' );
--- wp-content/themes/power-for-today/index.php
+++ wp-content/themes/power-for-today/index.php
@@ -9,35 +9,17 @@ get_header(); ?>
 
 <main>
   <section>
-    <h2 class="font-serif font-bold text-2xl md:text-5xl lg:text-6xl leading-tight pt-12 mb-16"><?php the_field('homepage_heading', 'options'); ?></h2>
+    <h2 class="font-serif font-bold text-2xl sm:text-3xl md:text-5xl lg:text-6xl leading-tight pt-12 mb-16"><?php the_field('homepage_heading', 'options'); ?></h2>
     <div class="flex flex-col md:flex-row border-t border-black">
       <div class="flex-no-shrink md:w-1/2 lg:w-5/12 py-12 md:pr-12 border-b md:border-b-0 md:border-r border-black">
         <h3 class="uppercase font-bold mb-6 tracking-wide">About This Blog</h3>
         <div class="about"><?php the_field('homepage_about', 'options'); ?></div>
       </div>
-      <div class="flex-1 py-12 md:pl-12">
-        <div class="flex items-end justify-between mb-6">
-          <h3 class="uppercase font-bold tracking-wide">Most Recent Articles</h3>
-          <p class="hidden md:block"><a class="hover:underline" href="#articles">View All Articles</a></p>
+      <div class="flex-1 my-12 md:ml-12 relative">
+        <div class="md:absolute inset-0 w-full h-full">
+          <img class="w-full h-full object-cover" src="<?php the_field('homepage_image', 'options'); ?>" alt="Desk Image">
         </div>
-        <?php if ( ! empty( $recent_posts ) ) : ?>
-          <ul>
-            <?php foreach ( $recent_posts as $rp ) : ?>
-              <li>
-                <a class="block py-4 hover:bg-grey-100 -mx-4 px-4" href="<?php echo esc_attr( get_permalink( $rp['ID'] ) ); ?>">
-                  <p class="font-serif text-xl lg:text-2xl mb-2"><?php echo esc_html( $rp['post_title'] ); ?></p>
-                  <div class="flex items-center text-grey-600 uppercase text-sm lg:text-base">
-                    <p><?php echo esc_html( get_the_author_meta( 'display_name', $rp['post_author'] ) ); ?></p>
-                    <span class="h-px w-12 mx-4 bg-grey-600"></span>
-                    <p><?php echo esc_html( date_i18n( 'M j, Y', strtotime( $rp['post_date'] ) ) ); ?></p>
-                  </div>
-                </a>
-              </li>
-            <?php endforeach; ?>
-          </ul>
-        <?php else : ?>
-          <div>Sorry, no posts to show!</div>
-        <?php endif; ?>
+        
       </div>
     </div>
   </section>
